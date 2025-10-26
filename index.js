6cca950074cb65b453e623629c2e92afcf0589b4
Nisacasa <nisacasa2@gmail.com>
2025-10-25 20:46:23 -0500

    v1.1

--- index.js
+++ index.js
@@ -2,25 +2,52 @@ const fechaHoy = new Date();
 const opciones = {day: '2-digit', month: '2-digit', year: 'numeric'}
 document.getElementById('fecha').textContent=fechaHoy.toLocaleDateString('es-Es', opciones)
 
-let vuelos = JSON.parse(localStorage.getItem('vuelos')||[])
+// Misiones
+let misiones = JSON.parse(localStorage.getItem('misiones')||[])
 
-function guardarVuelos(){
-    localStorage.setItem('vuelos', JSON.stringify(vuelos))
+function guardarmisiones(){
+    localStorage.setItem('misiones', JSON.stringify(misiones))
 }
 
-function mostrarVuelos(){
-    const lista = document.getElementById('listaVuelos')
+function mostrarmisiones(){
+    const lista = document.getElementById('listamisiones')
     lista.innerHTML = '';
-    vuelos.forEach(vuelo => {
+    misiones.forEach(mision => {
         const div = document.createElement('div')
         div.className = 'card';
         div.innerHTML = `
-        <span> Numero :${vuelo.numero} - Destino : ${vuelo.destino} -Estado: ${vuelo.estado}
-        <div class="actions">
-                <button onClick="editarVuelo(${index})"><i class="fa-solid fa-pencil"></i></button>
-                <button onClick="eliminarVuelo(${index})"><i class="fa-solid fa-trash"></i></button>
-                <button onClick="cambiarEstado(${index})"><i class="fa-solid fa-rotate"></i></button>
-        </div>
+        <span>${mision.nombre} - ${mision.objetivo} - Estado: ${mision.estado}</span>
+            <div class="actions">
+                <button title="editar(${index})"><i class="fa-solid fa-pencil"></i></button>
+                <button title="eliminar(${index})"><i class="fa-solid fa-trash"></i></button>
+                <button title="cambiarEstado(${index})"><i class="fa-solid fa-rotate"></i></button>
+            </div>
+        `
+
+        lista.appendChild(div);
+    });
+}
+
+// Vehículos
+let vehiculos = JSON.parse(localStorage.getItem('vehiculos')||[])
+
+function guardarvehiculos(){
+    localStorage.setItem('vehiculos', JSON.stringify(vehiculos))
+}
+
+function mostrarvehiculos(){
+    const lista = document.getElementById('listavehiculos')
+    lista.innerHTML = '';
+    vehiculos.forEach(vehiculo => {
+        const div = document.createElement('div')
+        div.className = 'card';
+        div.innerHTML = `
+        <span>${vehiculo.nombre} - ${vehiculo.tipo} - Matrícula: ${vehiculo.estado}</span>
+            <div class="actions">
+                <button title="editar(${index})"><i class="fa-solid fa-pencil"></i></button>
+                <button title="eliminar(${index})"><i class="fa-solid fa-trash"></i></button>
+                <button title="cambiarEstado(${index})"><i class="fa-solid fa-rotate"></i></button>
+            </div>
         `
 
         lista.appendChild(div);
